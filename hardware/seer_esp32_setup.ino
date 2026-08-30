af4db8990678d637d722c8bd86f49c6d53a1f4fe
Abdulbaset Saleh <155334992+asshaieas@users.noreply.github.com>
2026-08-30 21:21:08 +0300

    add see_esp32_setup.ino file

---
+++ hardware/seer_esp32_setup.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("SEER Smart School Bus System");
+  Serial.println("ESP32 development environment is ready!");
+}
+
+void loop() {
+  // Future SEER hardware code will run here 
+}
